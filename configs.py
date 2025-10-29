6eb648bcf224ae3bb7c3c0d43984e4679f2f0164
ShashankAgarwal1987 <shashank@lykke.travel>
2025-10-30 01:34:05 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,26 @@
+# configs.py
+
+COUNTRY_CONFIGS = {
+    "Egypt": {
+        "escalation_matrix": """
+Escalation Matrix India Office :
+Leader (Ms. Heba) : +201025267265
+Sales Leader (Lalit) : +918826894140
+
+Escalation Matrix Egypt Office :
+Egypt Operations Head (Ms. Reem Soud) : +2 01011900567
+Company CEO (Mr. Hossam) : +2 01116101030
+""",
+        "special_note": """
+Special Note on Egypt :
+* Total Payable tip for the trip : 120 USD [Days in Egypt * 5 * Number of Pax], Kindly pay the same upon arrival.
+* Tips are cultural and compulsory in Egypt. If you had to pay extra beyond the amount collected in advance, please intimate the team and the same will be refunded by the company.
+* Check-in/Check-out timing, Hotel bedding type etc. are controlled by respective accommodation only.
+""",
+        "other_comments": """
+Other Comments or Special Instructions :
+Above mentioned services are confirmed and non-changeable.
+Any changes would attract full cancellation of tours/services and additional tours/services will be charged as per company policies and directly payable in Egypt only.
+"""
+    }
+}
